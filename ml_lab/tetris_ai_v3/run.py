3dfaa94fbab5f5a7977ecb681e201c76d3d829ef
Shintaro Seki <s2pch.luck@gmail.com>
2020-02-11 03:46:56 +0900

    update

--- ml_lab/tetris_ai_v3/run.py
+++ ml_lab/tetris_ai_v3/run.py
@@ -17,53 +17,29 @@ logger = logging.getLogger(__name__)
 MACHINE_EPS = np.finfo(np.float32).eps.item()
 
 
-def run_episodes(model: M.TetrisModel, device: torch.device,
-                 num_episodes=10, max_steps=500,
-                 summary_writer=None):
-    optimizer = optim.RMSprop(model.parameters())
+def calc_loss(results: List[agent.StepResult], gamma=0.999):
+    # Discounted total rewards
+    expected_state_values = []
+    tmp = 0
+    for r in results[::-1]:
+        tmp = r.reward + gamma * tmp
+        expected_state_values.insert(0, tmp)
+    # Standarize
+    expected_state_values = torch.tensor(expected_state_values)
+    expected_state_values -= expected_state_values.mean()
+    expected_state_values /= (expected_state_values.std() + MACHINE_EPS)
 
-    for episode_id in range(num_episodes):
-        results: List[agent.StepResult] = []
+    policy_losses = []
+    state_value_losses = []
+    for r, value in zip(results, expected_state_values):
+        advantage = value - r.state_value
+        policy_losses.append(-r.action_log_prob * advantage)
+        state_value_losses.append(F.smooth_l1_loss(r.state_value, value))
 
-        def on_step_result(_step_i: int, r: agent.StepResult, _score: int):
-            results.append(r)
-            return True
-
-        num_steps, score, game = agent.run_steps(
-            model, device, max_steps=max_steps, step_result_cb=on_step_result)
-
-        logger.info('Episode {} => steps: {}, score: {}, game:\n{}'.format(
-            episode_id, num_steps, score, game))
-        if summary_writer is not None:
-            summary_writer.add_scalar('Episode/Steps', num_steps, episode_id)
-            summary_writer.add_scalar('Episode/Score', score, episode_id)
-
-        logger.info('Learning...')
-
-        expected_state_values = []
-        GAMMA = 0.999
-        tmp = 0
-        for r in results[::-1]:
-            tmp = r.reward + GAMMA * tmp
-            expected_state_values.insert(0, tmp)
-
-        expected_state_values = torch.tensor(expected_state_values)
-        expected_state_values -= expected_state_values.mean()
-        expected_state_values /= (expected_state_values.std() + MACHINE_EPS)
-
-        policy_losses = []
-        state_value_losses = []
-        for r, value in zip(results, expected_state_values):
-            advantage = value - r.state_value
-            policy_losses.append(-r.action_log_prob * advantage)
-            state_value_losses.append(F.smooth_l1_loss(r.state_value, value))
-
-        policy_losses = torch.stack(policy_losses)
-        state_value_losses = torch.stack(state_value_losses)
-        loss = policy_losses.sum() + state_value_losses.sum()
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    policy_losses = torch.stack(policy_losses)
+    state_value_losses = torch.stack(state_value_losses)
+    loss = policy_losses.sum() + state_value_losses.sum()
+    return loss
 
 
 def run(args: Optional[List[str]] = None):
@@ -74,7 +50,7 @@ def run(args: Optional[List[str]] = None):
     parser.add_argument('--tb_log_dir',
                         default='tmp/tetris_ai_v3/tb_log/{}'.format(now_str))
     parser.add_argument('-m', '--model', default='tmp/tetris_ai_v3/model.pt')
-    parser.add_argument('--num_iterations', default=1, type=int)
+    parser.add_argument('--learning_episode_interval', default=1, type=int)
     parser.add_argument('--num_episodes', default=5, type=int)
     parser.add_argument('--max_steps', default=500, type=int)
 
@@ -103,12 +79,32 @@ def run(args: Optional[List[str]] = None):
         model.load_state_dict(torch.load(model_file))
         logger.info('model state was loaded from {}.'.format(model_file))
     model.to(device)
+    # optimizer = optim.RMSprop(model.parameters())
+    optimizer = optim.Adam(model.parameters())
 
-    for i in range(args.num_iterations):
-        logger.info('iteration#{}'.format(i))
+    for episode_id in range(args.num_episodes):
+        results: List[agent.StepResult] = []
 
-        run_episodes(model, device, args.num_episodes, args.max_steps,
-                     summary_writer)
+        def on_step_result(_step_i: int, r: agent.StepResult, _score: int):
+            results.append(r)
+            return True
 
-        torch.save(model.state_dict(), model_file)
-        logger.info('model sate was saved to {}'.format(model_file))
+        num_steps, score, game = agent.run_steps(
+            model, device, max_steps=args.max_steps,
+            step_result_cb=on_step_result)
+
+        logger.info('Episode {} => steps: {}, score: {}, game:\n{}'.format(
+            episode_id, num_steps, score, game))
+        if summary_writer is not None:
+            summary_writer.add_scalar('Episode/Steps', num_steps, episode_id)
+            summary_writer.add_scalar('Episode/Score', score, episode_id)
+
+        if (episode_id + 1) % args.learning_episode_interval == 0:
+            logger.info('Learning...')
+            optimizer.zero_grad()
+            loss = calc_loss(results)
+            loss.backward()
+            optimizer.step()
+
+            torch.save(model.state_dict(), model_file)
+            logger.info('model sate was saved to {}'.format(model_file))
