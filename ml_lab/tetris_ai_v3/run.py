e87b9d1d734ffab68f1f4a0524ac2fd58f96ab2b
Shintaro Seki <s2pch.luck@gmail.com>
2020-02-12 00:33:49 +0900

    update

--- ml_lab/tetris_ai_v3/run.py
+++ ml_lab/tetris_ai_v3/run.py
@@ -16,48 +16,52 @@ logger = logging.getLogger(__name__)
 MACHINE_EPS = np.finfo(np.float32).eps.item()
 
 
-def calc_loss(results: List[agent.StepResult], gamma=0.999):
+def calc_loss(results: List[agent.StepResult], gamma=0.99):
     # Discounted total rewards
-    expected_values = []
+    rewards = []
     tmp = 0
     for r in results[::-1]:
         tmp = r.reward + gamma * tmp
-        expected_values.insert(0, tmp)
+        rewards.insert(0, tmp)
     # Standarize
-    expected_values = torch.tensor(expected_values)
-    expected_values -= expected_values.mean()
-    expected_values /= (expected_values.std() + MACHINE_EPS)
+    rewards = torch.tensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + MACHINE_EPS)
 
     policy_losses = []
     value_losses = []
-    for r, value in zip(results, expected_values):
-        advantage = value - r.state_value
+    for r, reward in zip(results, rewards):
+        advantage = reward - r.state_value
         policy_losses.append(-r.action_log_prob * advantage)
-        value_losses.append(F.smooth_l1_loss(r.state_value, value))
+        value_losses.append(F.smooth_l1_loss(r.state_value, reward))
 
     policy_losses = torch.stack(policy_losses)
     value_losses = torch.stack(value_losses)
-    return policy_losses.sum() + value_losses.sum()
+    loss = policy_losses.sum() + value_losses.sum()
+    return loss
 
 
 class Args(NamedTuple):
+    log_level: str
     log_file: str
     tb_log_dir: str
     model: str
     num_episodes: int
     max_steps: int
+    discount_rate: float
 
 
 def run(args: Optional[List[str]] = None):
     now_str = time.strftime('%Y%m%d_%H%M%S')
 
     parser = argparse.ArgumentParser(prog='PROG')
+    parser.add_argument('--log_level', default='INFO')
     parser.add_argument('--log_file', default='')
     parser.add_argument('--tb_log_dir',
                         default='tmp/tetris_ai_v3/tb_log/{}'.format(now_str))
     parser.add_argument('-m', '--model', default='tmp/tetris_ai_v3/model.pt')
     parser.add_argument('--num_episodes', default=5, type=int)
     parser.add_argument('--max_steps', default=500, type=int)
+    parser.add_argument('--discount_rate', default=0.99, type=float)
 
     args = parser.parse_args(args)  # type: Args
 
@@ -69,7 +73,7 @@ def run(args: Optional[List[str]] = None):
         os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
     log_format = '%(asctime)s %(levelname)s [%(name)s] %(message)s'
-    logging.basicConfig(level=logging.DEBUG, format=log_format,
+    logging.basicConfig(level=args.log_level.upper(), format=log_format,
                         filename=log_file)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -105,7 +109,7 @@ def run(args: Optional[List[str]] = None):
             summary_writer.add_scalar('Episode/Score', score, episode_id)
 
         logger.info('Learning...')
-        loss = calc_loss(results)
+        loss = calc_loss(results, gamma=args.discount_rate)
         if summary_writer:
             summary_writer.add_scalar('Episode/Loss', loss.item(), episode_id)
         logger.info('loss = {}'.format(loss.item()))
